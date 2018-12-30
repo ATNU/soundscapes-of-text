989679f99e18a781590999abce949bd3b663901e
Mark Turner <nmdt3@nmdt3.local>
2018-12-30 15:59:17 +0000

    fixed api url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://localhost:4200',
+  serverUrl: 'http://atnu.di-projects.net:4200',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
