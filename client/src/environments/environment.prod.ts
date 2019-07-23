5eaf6396ae92220a377927f35cde0db2bae8ad72
andywhardy <andywhardy@gmail.com>
2019-07-23 12:12:36 +0100

    updated production backend url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://soundscapes.atnu.di-projects.net',
+  serverUrl: 'https://soundscapes-of-text-server.azurewebsites.net',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
